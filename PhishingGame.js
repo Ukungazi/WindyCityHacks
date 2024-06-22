fa4f7d32378689d5c91cecc09b33721a457ee167
Ukungazi <89703158+Ukungazi@users.noreply.github.com>
2024-06-22 14:10:54 -0500

    adding text

--- PhishingGame.js
+++ PhishingGame.js
@@ -7,14 +7,28 @@ function wordCheck()
 {
     for (var i = 0; i<wordList.size; i++)
     {
-        wordScramble(wordList[i]);
-        String guess = Scanner.nextLine();
-        if ()
+        var scrambledWord = wordScramble(wordList[i])
+        document.getElementById("guessBox").textContent=scrambledWord;
+        var guess = 
+        if (scrambledWord == guess)
+        {
+            points += 500;
+        } else {
+            document.getElementById("guessBox").textContent='Incorrect. Next word...';
+        }
     }
 }
 function wordScramble(String word)
 {
+    if (word.length == 0)
+    {
+        return;
+    }
     int start = (Math.random() * word.length);
     String newWord = word.substring(0,start) + word.substring(start+1);
     return word.substring(start,start+1) + wordScramble(newWord);
+}
+function endGame()
+{
+    document.getElementById("guessBox").textContent='Incorrect. Next word...';
 }
